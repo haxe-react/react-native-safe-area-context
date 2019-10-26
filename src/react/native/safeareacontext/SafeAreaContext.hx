29916c4694681de6832218e38174ddc05f10be0f
serjek <sbalitskiy@gmail.com>
2019-10-26 10:47:26 +0200

    Update SafeAreaContext.hx
    typos fixed

--- src/react/native/safeareacontext/SafeAreaContext.hx
+++ src/react/native/safeareacontext/SafeAreaContext.hx
@@ -3,25 +3,19 @@ package react.native.safeareacontext;
 import react.native.component.props.ViewProps;
 import react.ReactComponent;
 
-@:jsRequire('react-native-safe-area-context', 'efault')
+@:jsRequire('react-native-safe-area-context')
 extern class SafeAreaContext {
 	static function useSafeArea():SafeAreaInset;
 }
 
 @:jsRequire('react-native-safe-area-context', 'SafeAreaProvider')
-extern class SafeAreaProvider extends ReactComponentOfProps<ViewProps> {}{
-	
-}
+extern class SafeAreaProvider extends ReactComponentOfProps<ViewProps> {}
 
 @:jsRequire('react-native-safe-area-context', 'SafeAreaConsumer')
-extern class SafeAreaConsumer extends ReactComponentOfProps<ViewProps> {}{
-	
-}
+extern class SafeAreaConsumer extends ReactComponentOfProps<ViewProps> {}
 
 @:jsRequire('react-native-safe-area-context', 'SafeAreaView')
-extern class SafeAreaView extends ReactComponentOfProps<ViewProps> {}{
-	
-}
+extern class SafeAreaView extends ReactComponentOfProps<ViewProps> {}
 
 typedef SafeAreaInset = {
 	top:Int,
